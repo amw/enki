b851b9c9f7dcc86e8778c06e3d099b27b7027739
Adam Wróbel <adam@fluxinc.ca>
2013-06-17 23:07:00 -0700

    Fix that charset error in ActionMailer init script.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,4 +1,3 @@
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.default_charset = "utf-8"
 ActionMailer::Base.default :from => "you@example.com"
